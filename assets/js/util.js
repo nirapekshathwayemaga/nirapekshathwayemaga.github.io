b650e404bd780fe97341e586e1473aebefd021ec
Chimerasam <106704752+Chimerasam@users.noreply.github.com>
2022-07-04 13:22:46 -0400

    added google analyitics for improving site navigation features

--- assets/js/util.js
+++ assets/js/util.js
@@ -585,3 +585,12 @@
 	};
 
 })(jQuery);
+
+
+try {
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'G-7GXJXYT3TE');
+  } catch (error) {
+  }
